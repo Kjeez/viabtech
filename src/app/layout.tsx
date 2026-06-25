9308a9297edf341f47145254751e1d19ba3d9e2f
kunal <konnectwithkunal@gmail.com>
2026-06-25 18:50:11 +0530

    Add Meta Pixel (1648874452849084)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,20 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-TDCVVC3M');`}
         </Script>
         {/* End Google Tag Manager */}
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1648874452849084');
+          fbq('track', 'PageView');`}
+        </Script>
+        {/* End Meta Pixel */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -83,6 +97,18 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1648874452849084&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
         <LanguageProvider>
           <a
             href="#main-content"
