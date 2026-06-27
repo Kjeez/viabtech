f09b7738ffaa492cf7ae4efcd2bcf5edf1dfbf7e
kunal <konnectwithkunal@gmail.com>
2026-06-27 20:57:51 +0530

    Update Meta Pixel ID to 1041313038421119

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1648874452849084');
+          fbq('init', '1041313038421119');
           fbq('track', 'PageView');`}
         </Script>
         {/* End Meta Pixel */}
@@ -104,7 +104,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1648874452849084&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1041313038421119&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
